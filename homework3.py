696af77a0814b4748faf0a1e74788bb69ad43540
elizamendibekova <75625969+elizamendibekova@users.noreply.github.com>
2020-12-09 15:37:23 +0600

    Add files via upload

--- homework3.py
+++ homework3.py
@@ -1,7 +1,8 @@
-def calc(a, b, c):
+def calc():
 	
-	number_1 = int(input("Please enter the first number: "))
-	number_2 = int(input("Please enter the second number: "))
+	a = int(input("Please enter the first number:"))
+	b = int(input("Please enter the second number:"))
+	c = int(input("operation"))
 
 	if operation == "+":
 		print("{} + {} = " .format(number_1, number_2))
@@ -21,3 +22,20 @@ def calc(a, b, c):
 
 	else:
 		print("You have typed a valid operator, please run the program again.")
+
+
+def add(a, b):
+	return a + b
+
+def subtraction(a, b):
+	return a - b
+
+def multiply(a, b):
+	return a * b
+
+def division(a, b):
+	return a / b
+
+
+	
+
