726df957382efba79cc93e3409cc42de59e0a422
elizamendibekova <75625969+elizamendibekova@users.noreply.github.com>
2020-12-14 11:02:53 +0600

    Add files via upload

---
+++ homework5/main.py
@@ -0,0 +1,13 @@
+from stack import *
+s = classStack()
+
+print(s. is_empty())
+s.push(4)
+s.push("yes")
+print(s.peek())
+s.push(True)
+print(s.is_empty())
+s.push(8.4)
+print(s.remove())
+print(s.remove())
+print(s.size())
